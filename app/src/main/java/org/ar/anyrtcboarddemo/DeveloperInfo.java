e86eb71e789594f2eccf0dd2acdae380e9b03d16
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-14 11:49:46 +0800

    更新代码

--- app/src/main/java/org/ar/anyrtcboarddemo/DeveloperInfo.java
+++ app/src/main/java/org/ar/anyrtcboarddemo/DeveloperInfo.java
@@ -5,13 +5,13 @@ package org.ar.anyrtcboarddemo;
  */
 public class DeveloperInfo {
 
-    public final static String DEVELOPERID = "95878050";
+    public final static String DEVELOPERID = "";
 
-    public final static String APPID = "anyrtcBO6kElS14kjS";
+    public final static String APPID = "";
 
-    public final static String APPKEY = "J/m2NjA26ypnW5C9eUmKP6zTyTwY3dZ0Fz3wxPZ+I6Y";
+    public final static String APPKEY = "";
 
-    public final static String APPTOKEN = "2572604a08e238bd2a749318900e16c4";
+    public final static String APPTOKEN = "";
 
     public final static String IMAGE1 = "http://oss.teameeting.cn/docs/140248771/20180929173501062526/document_20180929173501536430_2.jpg";
 
