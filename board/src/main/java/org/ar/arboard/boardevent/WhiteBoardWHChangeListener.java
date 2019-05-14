04799292cbdac5bd71710d46316ea0e02c784872
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-14 11:53:14 +0800

    更新代码

--- board/src/main/java/org/ar/arboard/boardevent/WhiteBoardWHChangeListener.java
+++
@@ -1,9 +0,0 @@
-package org.ar.arboard.boardevent;
-
-/**
- * Created by liuxiaozhong on 2018/11/10.
- */
-public interface WhiteBoardWHChangeListener {
-
-    void boardWDChange(int width,int height);
-}
